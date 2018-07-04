fbc368aa30d791942a538141e4ff8d37b38542f5
Brian Tobal <brian.tobal@gmail.com>
2018-07-04 20:48:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board 
+  print "   ", "|", "   ", "|", "   " 
+  puts "\n-----------\n"
+  print "   ", "|", "   ", "|", "   " 
+  puts "\n-----------\n"
+  print "   ", "|", "   ", "|", "   \n"
+end 
