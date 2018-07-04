0fcf6558d53feb78e601840c997be7f3a8fd1c08
Brian Tobal <brian.tobal@gmail.com>
2018-07-04 20:57:44 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board (board)
   print " #{board[0]} ", "|", "#{board[1]} ", "|", " #{board[2]} " 
   puts "\n-----------\n"
-  print " #{board[3]} ", "|", "#{board[4]} ", "|", " #{board[5]} " 
+  print " #{board[3]} ", "|", " #{board[4]}", "|", " #{board[5]} " 
   puts "\n-----------\n"
   print " #{board[6]} ", "|", "#{board[7]} ", "|", " #{board[8]} \n"
 end 
