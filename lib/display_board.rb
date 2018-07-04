2c10a31be0896fb7f6e81e91c30999e7c97e6725
Brian Tobal <brian.tobal@gmail.com>
2018-07-04 20:53:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,10 +12,10 @@ def display_board (board)
 end 
 
 def display_board (board)
-  print " #{board}  ", "|", " #{board}  ", "|", " #{board}  " 
+  print " #{board[1]}  ", "|", " #{board[2]}  ", "|", " #{board[3]}  " 
   puts "\n-----------\n"
-  print " #{board}  ", "|", "  #{board} ", "|", " #{board}  " 
+  print " #{board[4]}  ", "|", "  #{board[5]} ", "|", " #{board[6]}  " 
   puts "\n-----------\n"
-  print " #{board}  ", "|", " #{board}  ", "|", " #{board}  \n"
+  print " #{board[7]}  ", "|", " #{board[8]}  ", "|", " #{board[9]}  \n"
 end 
 
